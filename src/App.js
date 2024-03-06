aeb3a38e54eef7927f485f6adccd3436e36e3eb9
Rafael Castro <rafael.actt@gmail.com>
2024-03-06 18:52:14 -0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layouts/Container'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+   <Navbar/>
+      <Container customclass="min-height">
+         <Routes>
+
+        <Route path='/' exact element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/newproject' element={<NewProject />} />
+
+      </Routes> 
+      </Container>
+      <p>Footer</p>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,7 @@
+function Container(){
+    return(
+        <div></div>
+    )
+}
+
+export default Container
