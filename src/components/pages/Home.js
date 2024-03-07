5cd4d03cca440ae62a0a28029397fd27a6a80010
Rafael Castro <rafael.actt@gmail.com>
2024-03-06 22:03:23 -0300

    Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton'
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+    <section className={styles.home_container}>
+        <h1>Welcome to <span>Costs</span></h1>
+        <p>Start manage your projects right now!</p>
+        <LinkButton to="/newproject" text="Create Project" />
+        <img src={savings} alt="costs"/>
+    </section>)
 }
 
 export default Home
