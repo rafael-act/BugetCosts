547617c8c58407e2e0fd86642de17693f55f5a8b
Rafael Castro <rafael.actt@gmail.com>
2024-03-12 22:06:08 -0300

    iniciando o formulario de projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,15 @@
-function NewProject(){
-    return <h1>NewProject</h1>
+import styles from './NewProject.module.css';
+import ProjectForm from '../project/ProjectForm';
+
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>NewProject</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>Formulário</p>
+            <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProject
